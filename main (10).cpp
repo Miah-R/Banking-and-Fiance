edac1cbc4ff1686aef164e7a0ee1842709791f1c
KeyonnaP99 <97484410+KeyonnaP99@users.noreply.github.com>
2022-03-03 18:06:04 -0600

    Add files via upload

---
+++ main (10).cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+struct Node {
+   int data;
+   struct Node *next;
+};
+struct Node* head = NULL;
+void insert(int new_data) {
+   struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
+   new_node->data = new_data;
+   new_node->next = head;
+   head = new_node;
+}
+void display() {
+   struct Node* ptr;
+   ptr = head;
+   while (ptr != NULL) {
+      cout<< ptr->data <<" ";
+      ptr = ptr->next;
+   }
+}
+int main()
+{
+   insert(1);
+   cout<<"The app name:";
+   cout<<"the date:";
+   cout<<"transactions";
+   display();
+   return 0;
+}
